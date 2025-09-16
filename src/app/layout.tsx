8a3ae187b1d2fb78aa5d9d9c4388164128e9db99
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2025-09-16 22:32:08 +0700

    feat: add metadata, logo and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,15 @@
+import { APP_DESCRIPTION, APP_NAME } from '@/constants';
 import { roboto } from '@/styles/font';
 import '@/styles/globals.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME
+  },
+  description: APP_DESCRIPTION
+};
 
 export default function RootLayout({
   children
---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? 'Shark Tracker';
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ??
+  'Shark tracker the world no.1 basic simple expense tracker web application';
