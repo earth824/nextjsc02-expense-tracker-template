36a87eb6a34297307a54fb5f6f28107a985e412b
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2025-09-17 19:10:04 +0700

    feat(home page): add content to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
+import ExampleImage from '@/components/home/example-image';
+import { Button } from '@/components/ui/button';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Shark Tracker No.1 Expense Manager'
+};
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex items-center gap-12 h-full max-w-7xl mx-auto">
+      <div className="flex flex-col gap-8 w-2/5">
+        <h1 className="text-5xl font-bold">
+          Simple way to manage personal finances
+        </h1>
+        <p className="text-muted-foreground font-medium text-lg">
+          It takes seconds to record daily transactions. Put them into clear and
+          visualized categories such as Expense: Food, Shopping or Income:
+          Salary, Investment.
+        </p>
+        <Button className="self-start">Start for free</Button>
+      </div>
+      <div className="w-3/5">
+        <ExampleImage />
+      </div>
+    </div>
+  );
 }
