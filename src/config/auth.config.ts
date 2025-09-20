7d5875b31b494889a8baefa3da94c1765f84f6c9
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2025-09-20 08:48:15 +0700

    fix edge server

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -14,18 +14,19 @@ export const authConfig = {
   providers: [
     Credentials({
       async authorize(credentials) {
-        const { data, success } = signInSchema.safeParse(credentials);
-        if (!success) return null;
-        const user = await findUserByEmail(data.email);
-        if (!user) return null;
+        // const { data, success } = signInSchema.safeParse(credentials);
+        // if (!success) return null;
+        // const user = await findUserByEmail(data.email);
+        // if (!user) return null;
 
-        const isMatch = await bcrypt.compare(
-          data.password,
-          user.password ?? ''
-        );
-        if (!isMatch) return null;
+        // const isMatch = await bcrypt.compare(
+        //   data.password,
+        //   user.password ?? ''
+        // );
+        // if (!isMatch) return null;
 
-        return user;
+        // return user;
+        return null;
       }
     })
   ],
